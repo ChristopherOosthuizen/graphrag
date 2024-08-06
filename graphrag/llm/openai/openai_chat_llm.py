4e746639df610d0fa63fb823c2b72d2695784fbb
Christopher Oosthuizen <christopher.jan.oosthuizen@gmail.com>
2024-08-06 16:59:15 -0500

    Fixed output

--- graphrag/llm/openai/openai_chat_llm.py
+++ graphrag/llm/openai/openai_chat_llm.py
@@ -53,7 +53,8 @@ class OpenAIChatLLM(BaseLLM[CompletionInput, CompletionOutput]):
         completion = await self.client.chat.completions.create(
             messages=messages, **args
         )
-        open("output.txt","a").write(str(CompletionInput)+" \n"+str(completion.choices[0].text))
+        open("output.txt","a").write(str(input)+" \n"+str(completion.choices[0].text))
+        print(str(input)+" \n"+str(completion.choices[0].text))
         return completion.choices[0].message.content
 
     async def _invoke_json(
